1984c1981d5781b98e3c0e7843b9e5c57b51e0e6
Abdeev Vladimir <107181781+abdeev@users.noreply.github.com>
2022-09-06 16:57:08 +0300

    adding files, package.json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const divElement = document.querySelector('.gallery');
+
+galleryItems.forEach(i => {
+    const { preview, original, description } = galleryItems;
+    const galleryItemElement = `
+    <a class="gallery__item" href= "${i.original}">
+        <img
+            class="gallery__image"
+            src="${i.preview}"
+            alt="${i.description}"
+        />
+    </a>`
+
+    divElement.insertAdjacentHTML('beforeend', galleryItemElement);
+})
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250, navText: ['P R E V ←','N E X T →'], overlayOpacity: 0.8});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+player.on('timeupdate', function(data) {
+    // iframe.setAttribute.dataset.jsTime = data.seconds;
+    const throttledVideoPlaying = _.throttle(console.log, 1000);
+
+iframe.addEventListener('play', throttledVideoPlaying);
+    console.log(data.seconds);
+});
+
+
+// player.setCurrentTime(videoplayerCurrentTime).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
